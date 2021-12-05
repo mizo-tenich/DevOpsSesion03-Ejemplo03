1a7753d7c6c52d2d8acba8588b248ea3063ab624
Misael <misael.tenorio@payclip.com>
2021-12-05 01:28:10 -0600

    Actualizacion de linea en impresion

--- python/app.py
+++ python/app.py
@@ -1,8 +1,6 @@
-# This program uses the return value of a function.
-
 def main():
     # Print a single text
-    print("Hola Bedu!")
+    print("Hola Bedu! y futuros expertos DevOps!!!")
 
 # Call the main function.
 main()
